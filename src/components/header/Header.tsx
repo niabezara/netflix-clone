7e7763734d37643eecee7c9fc34258ca7f37519e
niabezara <niabezarashvili@yahoo.com>
2024-01-18 19:14:39 +0400

    button shadow

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -14,32 +14,12 @@ export default function Header() {
   const toggleSidebar = () => {
     setIsMenuOpen(!isMenuOpen);
   };
-  // const pathname = usePathname();
-  // const router = useRouter();
-  // const locale = useLocale();
 
-  // const onLinkClickitem = (selectedValue: "ka" | "en") => {
-  //   startTransition(() => {
-  //     router.replace(pathname, { locale: selectedValue });
-  //   });
-  // };
   return (
     <header className="px-8 py-6 w-full absolute z-10 flex justify-between items-center">
       <Icons.logo width="155px" height="55px" />
       <div className="flex gap-4">
         <LocaleSwitcher isMenuOpen={isMenuOpen} />
-        {/* <select
-          id="local"
-          className="pr-4 bg-transparent text-white border-[#5E5E5E] border-[1px] rounded-sm py-1 px-4 mr-3"
-          onChange={(e) => onLinkClickitem(e.target.value as "ka" | "en")}
-        >
-          <option value="en" className="text-black">
-            English
-          </option>
-          <option value="ka" className="text-black">
-            Georgian
-          </option>
-        </select> */}
         <Link href="/auth">
           <button className="bg-red-600 text-white p-1 px-2 rounded-md cursor-pointer">
             Sign in
