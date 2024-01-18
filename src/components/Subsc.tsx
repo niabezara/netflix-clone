2a71ca799adad205999ae49e60e525952fec950b
niabezara <niabezarashvili@yahoo.com>
2024-01-18 15:06:44 +0400

    border

--- src/components/Subsc.tsx
+++ src/components/Subsc.tsx
@@ -4,6 +4,7 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { EmailValidationSchema, emailValidationSchema } from "@/utils/schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useTranslations } from "next-intl";
+import { cn } from "@/utils";
 
 interface IFormInput {
   email: string;
@@ -21,15 +22,31 @@ export default function Subsc() {
     },
   });
   const t = useTranslations("Email");
-  const onSubmit = async () => {};
+  const onSubmit = async () => {
+    console.log("click");
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex gap-3  mt-4">
-      <div className="inputField md:w-[427px]">
-        <input {...register("email")} />
+    <form onSubmit={handleSubmit(onSubmit)} className="flex gap-3  mt-4 z-40">
+      <div
+        className={cn(
+          "inputField md:w-[427px]",
+          errors.email && "border-b-2 rounded-[6px] border-amber-600"
+        )}
+      >
+        <input
+          type="text"
+          id="email"
+          placeholder=""
+          {...register("email", { required: true })}
+          autoComplete="off"
+        />
         <label>{t("email")}</label>
       </div>
-      <button className="bg-red-600 flex items-center text-white px-4 py-2 rounded-sm cursor-pointer text-[24px] ">
+      <button
+        className="bg-red-600 flex items-center text-white px-4 py-2 rounded-sm cursor-pointer text-[24px] hover:bg-red-500"
+        type="submit"
+      >
         {t("start")}
         <IoIosArrowForward />
       </button>
